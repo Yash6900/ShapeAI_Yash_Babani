d74c57268e6a84abfacd7c6a6bf74535add817c1
Yash6900 <83388271+Yash6900@users.noreply.github.com>
2021-05-17 19:32:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/",function (req,res) {
 app.post("/" , function(req, res){
     var n1=Number(req.body.num1);
     var n2=Number(req.body.num2);
-    var result = n1/(n1+n2);
+    var result = n1/(n2*n2);
    
     res.send("Your BMI is"+" " +result);
 });
